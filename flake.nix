242c3386740307444241b170ea7bad1f21299bce
Konstantin Larionov <lkb@mail.ru>
2026-02-21 14:57:15 +0300

    Add async-profiler to direnv config

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 # flake.nix
 {
 
-  description = "Java 11 + Maven development environment (Flake)";
+  description = "Java 11 + Maven development environment (Flake) + Async-profiler";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
@@ -13,19 +13,23 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        asyncProfilerLib = "${pkgs.async-profiler}/lib/libasyncProfiler.so";
       in
       {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             jdk11_headless
             maven
+            async-profiler
           ];
 
           shellHook = ''
+            export JAVA_HOME=${pkgs.jdk11_headless.home}
+            export ASYNC_PROFILER_LIB=${asyncProfilerLib}
             echo "Java 11 Environment"
             echo "Java: $(java -version 2>&1 | head -1)"
             echo "Maven: $(mvn --version | head -1)"
-            export JAVA_HOME=${pkgs.jdk11_headless.home}
+            echo "Async Profiler lib (ASYNC_PROFILER_LIB): $ASYNC_PROFILER_LIB"
           '';
         };
       }
